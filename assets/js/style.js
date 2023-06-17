731226f2ea2c598fd46718fa35b87ed95d94eab9
RobinSharma-05 <robin9253370222@gmail.com>
2023-06-17 12:20:10 +0530

    margin-changes

--- assets/js/style.js
+++ assets/js/style.js
@@ -61,3 +61,20 @@ $(".slick-slider-2").slick({
 // verticalSwiping: true,
 // swipeToSlide: true,
 // focusOnSelect: true,
+
+// /////////back to top///////////////
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
